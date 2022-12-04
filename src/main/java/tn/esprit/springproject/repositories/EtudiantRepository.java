df91b202edf98d22a6142c769983bc4bc9307a07
sofienbensalem <60828973+sofienbensalem@users.noreply.github.com>
2022-12-04 12:00:06 +0100

    Added Advanced Functions

--- src/main/java/tn/esprit/springproject/repositories/EtudiantRepository.java
+++ src/main/java/tn/esprit/springproject/repositories/EtudiantRepository.java
@@ -3,7 +3,9 @@ package tn.esprit.springproject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.repository.query.Param;
 import tn.esprit.springproject.entities.Etudiant;
+import tn.esprit.springproject.entities.Option;
 
 import java.util.List;
 
@@ -17,4 +19,9 @@ public interface EtudiantRepository extends JpaRepository<Etudiant,Integer> {
     @Query(value = "select count (e.idEtudiant) from Etudiant e group by e.departement.idDepart having e.departement.idDepart=?1")
     Integer nombreEtudiantsByDepartement(int idDepart);
 
+    @Query(value = "select count (e.idEtudiant) from Etudiant e where e.op=:option")
+    Integer nombreEtudiantsByOption(@Param("option") Option option);
+
+    @Query(value = "select e from Etudiant e where e.op=:option")
+    List<Etudiant> EtudiantsByOption(@Param("option") Option option);
 }
